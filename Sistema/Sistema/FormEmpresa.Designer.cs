95ce6bc7ac79d6c3621c9d2d1d4861ce7e9212d1
Matheus-Borges <84951351+Matheus-Borges-Never@users.noreply.github.com>
2022-06-27 20:41:12 -0300

    Second commit

---
+++ Sistema/Sistema/FormEmpresa.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace Sistema
+{
+    partial class FormEmpresa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Sistem = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtRua = new System.Windows.Forms.TextBox();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.mtbCep = new System.Windows.Forms.MaskedTextBox();
+            this.mtbTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.txtComplemento = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEmpresa = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Sistem
+            // 
+            this.Sistem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Sistem.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sistem.Location = new System.Drawing.Point(295, 30);
+            this.Sistem.Name = "Sistem";
+            this.Sistem.Size = new System.Drawing.Size(367, 38);
+            this.Sistem.TabIndex = 8;
+            this.Sistem.Text = "Cadastre-se";
+            this.Sistem.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.txtRua);
+            this.panel1.Controls.Add(this.txtBairro);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.mtbCep);
+            this.panel1.Controls.Add(this.mtbTelefone);
+            this.panel1.Controls.Add(this.txtComplemento);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.btnLogin);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtNumero);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtSenha);
+            this.panel1.Controls.Add(this.btnCadastrar);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtEmpresa);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(119, 125);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(722, 243);
+            this.panel1.TabIndex = 7;
+            // 
+            // txtRua
+            // 
+            this.txtRua.Location = new System.Drawing.Point(218, 88);
+            this.txtRua.Name = "txtRua";
+            this.txtRua.Size = new System.Drawing.Size(161, 20);
+            this.txtRua.TabIndex = 9;
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Location = new System.Drawing.Point(455, 88);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(161, 20);
+            this.txtBairro.TabIndex = 20;
+            // 
+            // label8
+            // 
+            this.label8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label8.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(361, 85);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(106, 38);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Bairro:";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // mtbCep
+            // 
+            this.mtbCep.Location = new System.Drawing.Point(75, 88);
+            this.mtbCep.Mask = "00000-000";
+            this.mtbCep.Name = "mtbCep";
+            this.mtbCep.Size = new System.Drawing.Size(89, 20);
+            this.mtbCep.TabIndex = 18;
+            this.mtbCep.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            // 
+            // mtbTelefone
+            // 
+            this.mtbTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtbTelefone.Location = new System.Drawing.Point(466, 134);
+            this.mtbTelefone.Mask = "(00)00000-0000";
+            this.mtbTelefone.Name = "mtbTelefone";
+            this.mtbTelefone.Size = new System.Drawing.Size(122, 22);
+            this.mtbTelefone.TabIndex = 17;
+            this.mtbTelefone.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            // 
+            // txtComplemento
+            // 
+            this.txtComplemento.Location = new System.Drawing.Point(170, 137);
+            this.txtComplemento.Name = "txtComplemento";
+            this.txtComplemento.Size = new System.Drawing.Size(161, 20);
+            this.txtComplemento.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label7.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(37, 137);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(127, 38);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Complemento:";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Calisto MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnLogin.Location = new System.Drawing.Point(358, 194);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(115, 35);
+            this.btnLogin.TabIndex = 14;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label3
+            // 
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(385, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 26);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Telefone:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(661, 85);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(55, 20);
+            this.txtNumero.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label6.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(613, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 38);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Nº:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label5
+            // 
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label5.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(136, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 38);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Rua:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 38);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "CEP:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(422, 34);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(185, 20);
+            this.txtSenha.TabIndex = 5;
+            this.txtSenha.Text = "\r\n";
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.BackColor = System.Drawing.Color.Red;
+            this.btnCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadastrar.Font = new System.Drawing.Font("Calisto MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCadastrar.Location = new System.Drawing.Point(198, 194);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(115, 35);
+            this.btnCadastrar.TabIndex = 5;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = false;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // label4
+            // 
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(354, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 38);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Senha:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtEmpresa
+            // 
+            this.txtEmpresa.Location = new System.Drawing.Point(149, 34);
+            this.txtEmpresa.Name = "txtEmpresa";
+            this.txtEmpresa.Size = new System.Drawing.Size(199, 20);
+            this.txtEmpresa.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 38);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Empresa:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // FormEmpresa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(959, 492);
+            this.Controls.Add(this.Sistem);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormEmpresa";
+            this.Text = "FormEmpresa";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Sistem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtRua;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.MaskedTextBox mtbCep;
+        private System.Windows.Forms.MaskedTextBox mtbTelefone;
+        private System.Windows.Forms.TextBox txtComplemento;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEmpresa;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Sistema/Sistema/FormEmpresa.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Sistema
+{
+    public partial class FormEmpresa : Form
+    {
+        //Referencia para conexão
+        SqlConnection conexao = new SqlConnection(@"Data Source = DESKTOP-SC2GFN8; Initial Catalog = sistemaEmpresarial; Integrated Security = True");
+
+        public FormEmpresa()
+        {
+            InitializeComponent();
+            txtEmpresa.Select();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            if (conexao.State == ConnectionState.Closed)
+            {
+                conexao.Open();
+            }
+            if (conexao.State == ConnectionState.Open)
+            {
+                SqlCommand cm;
+                string sql = "";
+                sql = "insert into empresa values('" + txtEmpresa.Text + "','" + txtSenha.Text + "','" + txtRua.Text + "','" + txtBairro.Text + "','" + txtComplemento.Text + "','" + mtbCep.Text + "','" + txtNumero.Text + "','" + mtbTelefone.Text + "')";
+                cm = new SqlCommand(sql, conexao);
+
+                int ret = cm.ExecuteNonQuery();
+                if (ret > 0)
+                {
+                    MessageBox.Show("Empresa inserida com sucesso!");
+                    txtEmpresa.Text = "";
+                    txtSenha.Text = "";
+                    txtRua.Text = "";
+                    txtBairro.Text = "";
+                    txtComplemento.Text = "";
+                    mtbCep.Text = "";
+                    txtNumero.Text = "";
+                    mtbTelefone.Text = "";
+
+                }
+                else
+                {
+                    MessageBox.Show("Usuario já existe!!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            Form1 index = new Form1(); //Chama  tela index
+            this.Hide();
+            index.Show();
+        }
+    }
+}
